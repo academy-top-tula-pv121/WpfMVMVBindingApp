6de16e8e4cacba292c0e3afde67c02ab65535af7
maxefimov <efimov.top.academy@gmail.com>
2022-11-21 21:19:53 +0300

    Добавьте файлы проекта.

---
+++ DataSource.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class DataSource : INotifyPropertyChanged
+    {
+        private User userSelect;
+
+        public User UserSelect
+        {
+            set
+            {
+                userSelect = value;
+                OnPropertyChanged(nameof(UserSelect));
+            }
+            get => userSelect;
+        }
+        public ObservableCollection<User> Users { set; get; }
+        public DataSource()
+        {
+            Users = new ObservableCollection<User>
+            {
+                new User("Bob", "Yandex"),
+                new User("Tim", "Mail Group"),
+                new User("Sam", "Ozon"),
+                new User("Joe", "Yandex")
+            };
+                
+        }
+       
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class User : INotifyPropertyChanged
+    {
+        private string name;
+        private string company;
+        public string Name { 
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+            get => name; }
+        public string Company 
+        { set
+            {
+                company = value;
+                OnPropertyChanged(nameof(Company));
+            }
+            get => company;
+        }
+
+        public User(string name = "", string company = "")
+        {
+            Name = name;
+            Company = company;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
